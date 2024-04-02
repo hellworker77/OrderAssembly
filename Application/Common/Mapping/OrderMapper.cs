271a43c2c5a91fcf0480c342b6b1f303ea7155b3
king lich <iljapuskov@gmail.com>
2024-04-02 15:57:37 +0200

    feat mapping and interfaces

---
+++ Application/Common/Mapping/OrderMapper.cs
@@ -0,0 +1,13 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+using Mapster;
+
+namespace Application.Common.Mapping;
+
+public class OrderMapper : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<Order, OrderDto>();
+    }
+}
---
+++ Application/Interfaces/Repositories/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories;
+
+public interface IOrderRepository
+{
+    IList<Order> GetOrdersByIds(params int[] ids);
+}
